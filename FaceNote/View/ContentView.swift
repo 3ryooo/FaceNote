ddb3b02d916893e0df8d60a7a9d5d319c18cef9f
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-17 11:53:51 +0900

    add: ContentView-ViewModelを作成し、一部コードを移動

--- FaceNote/View/ContentView.swift
+++ FaceNote/View/ContentView.swift
@@ -8,39 +8,11 @@ import SwiftUI
 import PhotosUI
 import SwiftUI
 
-@Observable
-class FaceStore: Codable {
-    var items: [Face] = [].sorted()
-    
-    
-    private var saveURL: URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0].appendingPathComponent("items.json")
-    }
-    
-    func saveItems() {
-        do {
-            let data = try JSONEncoder().encode(items)
-            try data.write(to: saveURL, options: [.atomic, .completeFileProtection])
-        } catch {
-            print("保存失敗 \(error)")
-        }
-    }
-    
-    func loadItems() {
-        do {
-            let data = try Data(contentsOf: saveURL)
-            items = try JSONDecoder().decode([Face].self, from: data)
-        } catch {
-            print("読み込み失敗\(error)")
-        }
-    }
-    
-}
-
 struct ContentView: View {
+    
+    @State private var viewModel = ViewModel()
     @State private var selectedItem: PhotosPickerItem?
-    @State private var store = FaceStore()
+
     
     var body: some View {
         
@@ -52,11 +24,11 @@ struct ContentView: View {
                 guard let data = try? await selectedItem?.loadTransferable(type: Data.self) else { return }
                 guard let uiImage = UIImage(data: data) else { return }
                 
-                store.items.append(Face(name: "test", photo: uiImage))
+                viewModel.store.items.append(Face(name: "test", photo: uiImage))
             }
         }
         List {
-            ForEach(store.items) { item in
+            ForEach(viewModel.store.items) { item in
                 HStack {
                     Text(item.name)
                     Spacer()
---
+++ FaceNote/ViewModel/ContentView-ViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView-ViewModel.swift
+//  FaceNote
+//
+
+import Foundation
+import PhotosUI
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        
+        var store = FaceStore()
+    }
+}
+
+@Observable
+class FaceStore: Codable {
+    var items: [Face] = [].sorted()
+    
+    
+    private var saveURL: URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0].appendingPathComponent("items.json")
+    }
+    
+    func saveItems() {
+        do {
+            let data = try JSONEncoder().encode(items)
+            try data.write(to: saveURL, options: [.atomic, .completeFileProtection])
+        } catch {
+            print("保存失敗 \(error)")
+        }
+    }
+    
+    func loadItems() {
+        do {
+            let data = try Data(contentsOf: saveURL)
+            items = try JSONDecoder().decode([Face].self, from: data)
+        } catch {
+            print("読み込み失敗\(error)")
+        }
+    }
+    
+}
